34a5d3b63597691311220d9934c905622040529c
Yuki <yuki0104@protonmail.com>
2021-03-09 20:08:34 +0900

    second commit

--- cpu.py
+++ cpu.py
@@ -3,8 +3,7 @@ import subprocess
 import time
 import datetime
 import csv
-import pandas as pd
-import os
+import sys
 
 cmd=["vcgencmd", "measure_temp"]
 
@@ -21,14 +20,10 @@ while True:
   try:
     pr = subprocess.run(cmd, stdout=subprocess.PIPE)
     print(f"{datetime.datetime.now()}: {pr.stdout.decode('utf8')}")
-    #w.csv.writter(f)
-    #w.writerow([datetime.datetime.now(),pr.stdout.decode('utf8')])
-    ##df=pd.DataFrame([str(datetime.datetime.now()).rstrip(), pr.stdout.decode('utf8').rstrip()])
-    ##df.to_csv('out.csv')
     fl.write(str(datetime.datetime.now()).rstrip()+"," +pr.stdout.decode('utf8').rstrip())
     fl.write('\n')
     time.sleep(1)
   except KeyboardInterrupt:
       print("stop!")
       fl.close()
-      os.exit(0)
+      sys.exit(0)
