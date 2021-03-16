85f623809ab2556437d2bd9f3f3bf30a532a3802
Yuki <yuki0104@protonmail.com>
2021-03-16 18:42:21 +0900

    A commit

--- cpu.py
+++ cpu.py
@@ -8,20 +8,17 @@ import sys
 cmd=["vcgencmd", "measure_temp"]
 
 global pr
-#global f
-#global w
-#f=open("out.csv","w")
-#w=csv.writter(f)
-#global write
+global write
 global fl
-fl=open('out.txt','w')
+file_name = f"{datetime.datetime.now()}.txt"
+fl=open(file_name,'w')
 #write=csv.writter('out.csv')
 while True:
   try:
     pr = subprocess.run(cmd, stdout=subprocess.PIPE)
     print(f"{datetime.datetime.now()}: {pr.stdout.decode('utf8')}")
     fl.write(str(datetime.datetime.now()).rstrip()+"," +pr.stdout.decode('utf8').rstrip())
-    fl.write('\n')
+    fl.write("\n")
     time.sleep(1)
   except KeyboardInterrupt:
       print("stop!")
